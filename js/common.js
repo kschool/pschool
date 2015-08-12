d0ecaaee06c31d6b1af03c701117ccaaa60f6151
kschool <krishnadesigner612@gmail.com>
2015-08-12 16:47:03 +0530

    initial commit 1.0

--- js/common.js
+++ js/common.js
@@ -1,1 +1,27 @@
-test js
+var header = {
+	toggleMenu : function(){
+
+		$('.menu_icon').on('click',function(){
+			if($(this).hasClass('glyphicon-menu-hamburger')) {
+				alert(0);
+                $(this).removeClass('glyphicon-menu-hamburger');   
+                 $(this).addClass('glyphicon-remove');              
+            }
+            if($(this).hasClass('glyphicon-remove')) {
+                $(this).addClass('glyphicon-menu-hamburger').removeClass('glyphicon-remove');              
+            }
+			// $(this).hasClass("glyphicon-menu-hamburger").removeClass('glyphicon-menu-hamburger').addClass('glyphicon-remove');
+			// $(this).hasClass("glyphicon-remove").removeClass('glyphicon-remove').addClass('glyphicon-menu-hamburger');
+		})
+	}
+}
+
+
+
+
+
+
+
+$(function(){
+	header.toggleMenu();
+});
