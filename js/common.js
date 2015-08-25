630cc35be313c34b2f9ac99aaa46e4baccdd5c74
kschool <krishnadesigner612@gmail.com>
2015-08-25 20:56:07 +0530

    Home page done.

--- js/common.js
+++ js/common.js
@@ -1,18 +1,6 @@
-var header = {
-	toggleMenu : function(){
-
-		$('.menu_icon').on('click',function(){
-			if($(this).hasClass('glyphicon-menu-hamburger')) {
-				alert(0);
-                $(this).removeClass('glyphicon-menu-hamburger');   
-                 $(this).addClass('glyphicon-remove');              
-            }
-            if($(this).hasClass('glyphicon-remove')) {
-                $(this).addClass('glyphicon-menu-hamburger').removeClass('glyphicon-remove');              
-            }
-			// $(this).hasClass("glyphicon-menu-hamburger").removeClass('glyphicon-menu-hamburger').addClass('glyphicon-remove');
-			// $(this).hasClass("glyphicon-remove").removeClass('glyphicon-remove').addClass('glyphicon-menu-hamburger');
-		})
+var slider = {
+	homeSlider: function(){
+		$("#homePageSlider").bxSlider();
 	}
 }
 
@@ -20,8 +8,6 @@ var header = {
 
 
 
-
-
 $(function(){
-	header.toggleMenu();
+	slider.homeSlider();
 });
